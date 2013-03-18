42737919978fe250de9b1344ffd8dc0a345eabd5
Joseph Crawford <info@josephcrawford.com>
2013-03-18 18:01:14 -0400

    adding some exception handling

--- status.rb
+++ status.rb
@@ -1,16 +1,23 @@
 #!/usr/local/bin/ruby
 
 require 'net/http'
-while TRUE
-  url = URI.parse(ARGV[0])
-  req = Net::HTTP.new(url.host)
-  req.start() do |req|
-    x = req.head(url.path)
-    if x.code == "200" or x.code == "204"
-      puts "up"
-    else
-      puts "down"
+
+begin 
+  while TRUE
+    url = URI.parse(ARGV[0])
+    req = Net::HTTP.new(url.host)
+    req.start() do |req|
+      x = req.head(url.path)
+      if x.code == "200" or x.code == "204"
+        puts "up"
+      else
+        puts "down"
+      end
     end
+    sleep(60)
   end
-  sleep(60)
+rescue Exception => e
+  puts "Invalid URL Specified"
+  puts "Exception Message: " + e.message
 end
+
